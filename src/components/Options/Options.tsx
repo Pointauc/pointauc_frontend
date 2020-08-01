e6ef580b593be2eb31935948a20be1a538d3d420
Ankirig <qwertzxc0000@gmail.com>
2020-08-01 21:11:11 +0300

    feature: add options menu

--- src/components/Options/Options.tsx
+++ src/components/Options/Options.tsx
@@ -1,14 +1,22 @@
-import React, { useEffect } from 'react';
+import React, { MouseEvent, ReactNode, useEffect } from 'react';
 import './Options.scss';
 import { useDispatch } from 'react-redux';
 import Cookies from 'js-cookie';
-import { Paper } from '@material-ui/core';
+import { Fade, IconButton, Paper, Popper } from '@material-ui/core';
+import SettingsIcon from '@material-ui/icons/Settings';
 import TwitchLogin from '../TwitchLogin/TwitchLogin';
 import { setUsername } from '../../reducers/User/User';
 import { USERNAME_COOKIE_KEY } from '../../constants/common.constants';
 
 const Options: React.FC = () => {
   const dispatch = useDispatch();
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const toggleOptions = (e: MouseEvent<HTMLButtonElement>): void => {
+    setAnchorEl(anchorEl ? null : e.currentTarget);
+  };
+
+  const open = Boolean(anchorEl);
 
   useEffect(() => {
     const username = Cookies.get(USERNAME_COOKIE_KEY);
@@ -18,9 +26,20 @@ const Options: React.FC = () => {
   }, [dispatch]);
 
   return (
-    <Paper variant="outlined" className="options">
-      <TwitchLogin />
-    </Paper>
+    <div className="options">
+      <Popper keepMounted open={open} anchorEl={anchorEl} placement="top" transition>
+        {({ TransitionProps }): ReactNode => (
+          <Fade {...TransitionProps} timeout={350}>
+            <Paper variant="outlined" className="options-menu">
+              <TwitchLogin />
+            </Paper>
+          </Fade>
+        )}
+      </Popper>
+      <IconButton onClick={toggleOptions}>
+        <SettingsIcon />
+      </IconButton>
+    </div>
   );
 };
 
