050da65b0b395e8de5f5f116546bf16d98885e2c
Ankirig <qwertzxc0000@gmail.com>
2020-10-20 14:20:16 +0300

    feature: add home page button styles

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Button, Paper } from '@material-ui/core';
 import ROUTES from '../../constants/routes.constants';
+import './HomePage.scss';
 
 const HomePage: React.FC = () => {
   return (
-    <div>
-      <Link to={ROUTES.AUC_PAGE}>Аукцион</Link>
-      <Link to={ROUTES.VIDEO_REQUESTS}>Видео реквесты</Link>
-      <Link to={ROUTES.CHAT_BOT}>Чат бот</Link>
-    </div>
+    <Paper className="home-page">
+      <Link to={ROUTES.AUC_PAGE}>
+        <Button variant="contained" color="primary" className="home-page-button">
+          Аукцион
+        </Button>
+      </Link>
+      <Link to={ROUTES.VIDEO_REQUESTS}>
+        <Button variant="contained" color="primary" className="home-page-button">
+          Видео реквесты
+        </Button>
+      </Link>
+      <Link to={ROUTES.CHAT_BOT}>
+        <Button variant="contained" color="primary" className="home-page-button">
+          Чат бот
+        </Button>
+      </Link>
+    </Paper>
   );
 };
 
