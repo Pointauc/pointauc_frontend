ff74eac1dc1f6ec850613f18e9d6ec0d12c7b28e
Ankirig <qwertzxc0000@gmail.com>
2021-02-16 18:46:45 +0300

    feature: handle subscribe errors

--- src/models/alert.model.ts
+++ src/models/alert.model.ts
@@ -1,10 +1,11 @@
 export enum AlertTypeEnum {
   Success = 'success',
+  Error = 'error',
 }
 
 export interface AlertProps {
   message: string;
-  type: AlertTypeEnum.Success;
+  type: AlertTypeEnum;
 }
 
 export interface AlertType extends AlertProps {
--- src/reducers/Subscription/Subscription.ts
+++ src/reducers/Subscription/Subscription.ts
@@ -2,6 +2,8 @@ import { createSlice, PayloadAction, ThunkDispatch } from '@reduxjs/toolkit';
 import { Action } from 'redux';
 import { RootState } from '../index';
 import { MESSAGE_TYPES, SERVER_MESSAGES } from '../../constants/webSocket.constants';
+import { addAlert } from '../notifications/notifications';
+import { AlertTypeEnum } from '../../models/alert.model';
 
 interface SubscribeState {
   actual: boolean;
@@ -57,6 +59,16 @@ export const sendCpSubscribedState = (isSubscribed: boolean) => (
       dispatch(setTwitchSubscribeState({ actual: false, loading: false }));
       webSocket?.removeEventListener('message', handleSubscribeResponse);
     }
+
+    if (responseType === SERVER_MESSAGES.CP_SUBSCRIBE_ERROR) {
+      dispatch(
+        addAlert({
+          message: 'Ошибка! Возможно у вас недоступны поинты на канале или есть награды с одинаковой стоимостью',
+          type: AlertTypeEnum.Error,
+        }),
+      );
+      dispatch(setTwitchSubscribeState({ actual: false, loading: false }));
+    }
   };
 
   webSocket?.addEventListener('message', handleSubscribeResponse);
@@ -82,6 +94,16 @@ export const sendDaSubscribedState = (isSubscribed: boolean) => (
     if (responseType === SERVER_MESSAGES.DA_UNSUBSCRIBED) {
       dispatch(setDaSubscribeState({ actual: false, loading: false }));
       webSocket?.removeEventListener('message', handleSubscribeResponse);
+    }
+
+    if (responseType === SERVER_MESSAGES.DA_SUBSCRIBE_ERROR) {
+      dispatch(
+        addAlert({
+          message: 'Ошибка! Я хз в чем проблема может быть с донатами. Напишите мне в телегу, пофикшу.',
+          type: AlertTypeEnum.Error,
+        }),
+      );
+      dispatch(setDaSubscribeState({ actual: false, loading: false }));
     }
   };
 
